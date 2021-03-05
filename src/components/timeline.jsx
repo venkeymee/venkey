34036c109ea584bf38f3dcbd5140fb9c356b480a
venkey <venkeymee@gmail.com>
2021-03-06 01:02:30 +0400

    updated content

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,8 +21,9 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
+                        <h2>Software Consultant <span>2018-present</span></h2>
+                        <p>Currently working with UAE based Airlines domain. Majar part of work has been into the 
+                          resolving technical challenges like improving performance, scalability etc. And also expertised in CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
                       </div>
                     </div>
                   </article>
@@ -32,8 +33,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                        <h2>Software Developer/Software Engineer <span>2013-2018</span></h2>
+                        <p>I have desigend and developed complex e-commerce systems which involves the all the phases of customer journey like products, cart,orders and payments ..etc and also Identify project risks and work effectively to mitigated them </p>
                       </div>
                     </div>
                   </article>
@@ -43,8 +44,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Higher and Intermediate  Education <span>2003-2015</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <h2>Graduate from JNTU-Hyd <span>2009-2013</span></h2>
+                        <p>I have graduated in B-Tech C.S.E.(Computer Science and Engineering) with distinction.I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p> 
                       </div>
                     </div>
                   </article>
